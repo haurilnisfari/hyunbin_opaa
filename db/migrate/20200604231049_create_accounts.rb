99f0dcddbc40a8e56846fb934dfca55e66aea134
Hauril Maulida Nisfari <hauril.nisfari@gmail.com>
2020-06-05 06:15:38 +0700

    generate account model

---
+++ db/migrate/20200604231049_create_accounts.rb
@@ -0,0 +1,13 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :username
+      t.string :email
+      t.text :bio
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
