402955357603a26237de604ceba8f8272a37fda9
Hauril Maulida Nisfari <hauril.nisfari@gmail.com>
2020-06-06 03:49:37 +0700

    Add account_id on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  belongs_to :account
   has_secure_password
 
   validates :name, presence: true, length: {minimum: 3}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_201201) do
+ActiveRecord::Schema.define(version: 2020_06_05_204452) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_201201) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "account_id"
   end
 
 end
